df38bfe0209b2531c5e04369dbfca469771336cb
Steven J. Li <StevenJLi@gmail.com>
2013-04-27 22:59:47 -0700

    finish post views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+
+  def show
+    @sub_forum = SubForum.find(params[:sub_forum_id])
+    @parent = @sub_forum.parent
+    @post = Post.find(params[:id])
+    @replies = @post.replies
+  end
+
+  def new
+    @sub_forum = SubForum.find(params[:sub_forum_id])
+    @parent = @sub_forum.parent
+    @post = Post.new
+  end
+
+end
