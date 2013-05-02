89dd54d2b2a66416e558fd33f5c3cc7e2a5d4326
Steven J. Li <StevenJLi@gmail.com>
2013-05-02 16:03:35 -0700

    finish subforum view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @metaforum = MetaForum.all.first
     @reply = Reply.new
     @sub_forum = SubForum.find(params[:sub_forum_id])
     @parent = @sub_forum.parent
