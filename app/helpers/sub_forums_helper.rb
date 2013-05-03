368d6ce15b220b3820017cef12ee82e72bd8ec0e
Steven J. Li <StevenJLi@gmail.com>
2013-05-02 18:30:05 -0700

    added date and time

--- app/helpers/sub_forums_helper.rb
+++ app/helpers/sub_forums_helper.rb
@@ -1,2 +1,14 @@
 module SubForumsHelper
+
+  def date_parse(dateobject) 
+    "#{dateobject.month}/#{dateobject.day}/#{dateobject.year}, #{time_parse(dateobject)}" 
+  end
+
+  def time_parse(date_object)
+    hour = date_object.hour 
+    minute = date_object.min
+    str = (hour > 12)? " AM" : " PM"
+    "#{hour % 12}:#{minute}" + str
+  end
+
 end
